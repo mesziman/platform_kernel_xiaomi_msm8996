24534ce01fb4980b8a221516e9aa6d79f36b3951
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-27 13:48:07 +0000

    sched, cpuidle: Track cpuidle state index in the scheduler
    The idle-state of each cpu is currently pointed to by rq->idle_state but
    there isn't any information in the struct cpuidle_state that can used to
    look up the idle-state energy model data stored in struct
    sched_group_energy. For this purpose is necessary to store the idle
    state index as well. Ideally, the idle-state data should be unified.
    
    Change-Id: Ib013757dd192b6f6f0be098b3909f42cf2fb723b
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -789,6 +789,7 @@ struct rq {
 #ifdef CONFIG_CPU_IDLE
 	/* Must be inspected within a rcu lock section */
 	struct cpuidle_state *idle_state;
+	int idle_state_idx;
 #endif
 };
 
@@ -1835,6 +1836,17 @@ static inline struct cpuidle_state *idle_get_state(struct rq *rq)
 	WARN_ON(!rcu_read_lock_held());
 	return rq->idle_state;
 }
+
+static inline void idle_set_state_idx(struct rq *rq, int idle_state_idx)
+{
+	rq->idle_state_idx = idle_state_idx;
+}
+
+static inline int idle_get_state_idx(struct rq *rq)
+{
+	WARN_ON(!rcu_read_lock_held());
+	return rq->idle_state_idx;
+}
 #else
 static inline void idle_set_state(struct rq *rq,
 				  struct cpuidle_state *idle_state)
@@ -1844,6 +1856,15 @@ static inline void idle_set_state(struct rq *rq,
 static inline struct cpuidle_state *idle_get_state(struct rq *rq)
 {
 	return NULL;
+}
+
+static inline void idle_set_state_idx(struct rq *rq, int idle_state_idx)
+{
+}
+
+static inline int idle_get_state_idx(struct rq *rq)
+{
+	return -1;
 }
 #endif
 
