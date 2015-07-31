215a5040e3b45e80e2d362a1d7004ca386f7c616
Peter Zijlstra <peterz@infradead.org>
2015-07-31 21:28:18 +0200

    sched/core: Introduce 'struct rq_flags'
    In order to be able to pass around more than just the IRQ flags in the
    future, add a rq_flags structure.
    
    No difference in code generation for the x86_64-defconfig build I
    tested.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -516,7 +516,7 @@ int schedtune_can_attach(struct cgroup_subsys_state *css,
 {
 	struct task_struct *task;
 	struct boost_groups *bg;
-	unsigned long irq_flags;
+	struct rq_flags rf;
 	unsigned int cpu;
 	struct rq *rq;
 	int src_bg; /* Source boost group index */
@@ -535,10 +535,10 @@ int schedtune_can_attach(struct cgroup_subsys_state *css,
 		 * conditions with migration code while the task is being
 		 * accounted
 		 */
-		rq = lock_rq_of(task, &irq_flags);
+		rq = lock_rq_of(task, &rf);
 
 		if (!task->on_rq) {
-			unlock_rq_of(rq, task, &irq_flags);
+			unlock_rq_of(rq, task, &rf);
 			continue;
 		}
 
@@ -559,7 +559,7 @@ int schedtune_can_attach(struct cgroup_subsys_state *css,
 		 */
 		if (unlikely(dst_bg == src_bg)) {
 			raw_spin_unlock(&bg->lock);
-			unlock_rq_of(rq, task, &irq_flags);
+			unlock_rq_of(rq, task, &rf);
 			continue;
 		}
 
@@ -580,7 +580,7 @@ int schedtune_can_attach(struct cgroup_subsys_state *css,
 		bg->boost_ts = now - SCHEDTUNE_BOOST_HOLD_NS;
 
 		raw_spin_unlock(&bg->lock);
-		unlock_rq_of(rq, task, &irq_flags);
+		unlock_rq_of(rq, task, &rf);
 	}
 
 	return 0;
@@ -639,7 +639,7 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu)
 void schedtune_exit_task(struct task_struct *tsk)
 {
 	struct schedtune *st;
-	unsigned long irq_flags;
+	struct rq_flags rf;
 	unsigned int cpu;
 	struct rq *rq;
 	int idx;
@@ -647,7 +647,7 @@ void schedtune_exit_task(struct task_struct *tsk)
 	if (!unlikely(schedtune_initialized))
 		return;
 
-	rq = lock_rq_of(tsk, &irq_flags);
+	rq = lock_rq_of(tsk, &rf);
 	rcu_read_lock();
 
 	cpu = cpu_of(rq);
@@ -656,7 +656,7 @@ void schedtune_exit_task(struct task_struct *tsk)
 	schedtune_tasks_update(tsk, cpu, idx, DEQUEUE_TASK);
 
 	rcu_read_unlock();
-	unlock_rq_of(rq, tsk, &irq_flags);
+	unlock_rq_of(rq, tsk, &rf);
 }
 
 int schedtune_cpu_boost(int cpu)
