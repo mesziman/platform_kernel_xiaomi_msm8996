babe99e0cc9676a7c66beeb129a7562b451342a9
Vikram Mulukutla <markivx@codeaurora.org>
2016-01-25 12:22:42 -0800

    sched: Revert HMP and some MSM specific features
    Eliminate the HMP extensions and load tracking added
    on top of the scheduler in order to bring in EAS. Also,
    disable some MSM specific features as well.
    
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -733,6 +733,7 @@ void do_exit(long code)
 	}
 
 	exit_signals(tsk);  /* sets PF_EXITING */
+<<<<<<< HEAD
 
 	sched_exit(tsk);
 
@@ -740,6 +741,8 @@ void do_exit(long code)
 		su_exit();
 	}
 
+=======
+>>>>>>> 573f31e... sched: Revert HMP and some MSM specific features
 	/*
 	 * tsk->flags are checked in the futex code to protect against
 	 * an exiting task cleaning up the robust pi futexes.
