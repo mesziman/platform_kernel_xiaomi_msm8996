82dfecc8587d9b68870e0e6cb6541b453e2ce387
elad.wexler@gmail.com <elad.wexler@gmail.com>
2017-12-29 11:03:37 +0200

    binder: Prefer __func__ to using hardcoded function name
    Coding style fixup
    
    Change-Id: Iaf7802dd661a1f8eebe34275fc3b12bd126cec65
    Signed-off-by: Elad Wexler <elad.wexler@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4866,66 +4866,66 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
     return 0;
 
 err_bad_arg:
-    pr_err("binder_mmap: %d %lx-%lx %s failed %d\n",
-           proc->pid, vma->vm_start, vma->vm_end, failure_string, ret);
-    return ret;
+	pr_err("%s: %d %lx-%lx %s failed %d\n", __func__,
+	       proc->pid, vma->vm_start, vma->vm_end, failure_string, ret);
+	return ret;
 }
 
 static int binder_open(struct inode *nodp, struct file *filp)
 {
-    struct binder_proc *proc;
-    struct binder_device *binder_dev;
+	struct binder_proc *proc;
+	struct binder_device *binder_dev;
 
-    binder_debug(BINDER_DEBUG_OPEN_CLOSE, "binder_open: %d:%d\n",
-	     current->group_leader->pid, current->pid);
+	binder_debug(BINDER_DEBUG_OPEN_CLOSE, "%s: %d:%d\n", __func__,
+		     current->group_leader->pid, current->pid);
 
-    proc = kzalloc(sizeof(*proc), GFP_KERNEL);
-    if (proc == NULL)
-	return -ENOMEM;
-    spin_lock_init(&proc->inner_lock);
-    spin_lock_init(&proc->outer_lock);
-    get_task_struct(current->group_leader);
-    proc->tsk = current->group_leader;
-    INIT_LIST_HEAD(&proc->todo);
-    if (binder_supported_policy(current->policy)) {
-	proc->default_priority.sched_policy = current->policy;
-	proc->default_priority.prio = current->normal_prio;
-    } else {
-	proc->default_priority.sched_policy = SCHED_NORMAL;
-	proc->default_priority.prio = NICE_TO_PRIO(0);
-    }
+	proc = kzalloc(sizeof(*proc), GFP_KERNEL);
+	if (proc == NULL)
+		return -ENOMEM;
+	spin_lock_init(&proc->inner_lock);
+	spin_lock_init(&proc->outer_lock);
+	get_task_struct(current->group_leader);
+	proc->tsk = current->group_leader;
+	INIT_LIST_HEAD(&proc->todo);
+	if (binder_supported_policy(current->policy)) {
+		proc->default_priority.sched_policy = current->policy;
+		proc->default_priority.prio = current->normal_prio;
+	} else {
+		proc->default_priority.sched_policy = SCHED_NORMAL;
+		proc->default_priority.prio = NICE_TO_PRIO(0);
+	}
 
-    binder_dev = container_of(filp->private_data, struct binder_device,
-		  miscdev);
-    proc->context = &binder_dev->context;
-    binder_alloc_init(&proc->alloc);
+	binder_dev = container_of(filp->private_data, struct binder_device,
+				  miscdev);
+	proc->context = &binder_dev->context;
+	binder_alloc_init(&proc->alloc);
 
-    binder_stats_created(BINDER_STAT_PROC);
-    proc->pid = current->group_leader->pid;
-    INIT_LIST_HEAD(&proc->delivered_death);
-    INIT_LIST_HEAD(&proc->waiting_threads);
-    filp->private_data = proc;
+	binder_stats_created(BINDER_STAT_PROC);
+	proc->pid = current->group_leader->pid;
+	INIT_LIST_HEAD(&proc->delivered_death);
+	INIT_LIST_HEAD(&proc->waiting_threads);
+	filp->private_data = proc;
 
-    mutex_lock(&binder_procs_lock);
-    hlist_add_head(&proc->proc_node, &binder_procs);
-    mutex_unlock(&binder_procs_lock);
+	mutex_lock(&binder_procs_lock);
+	hlist_add_head(&proc->proc_node, &binder_procs);
+	mutex_unlock(&binder_procs_lock);
 
-    if (binder_debugfs_dir_entry_proc) {
-	char strbuf[11];
+	if (binder_debugfs_dir_entry_proc) {
+		char strbuf[11];
 
-	snprintf(strbuf, sizeof(strbuf), "%u", proc->pid);
-	/*
-	 * proc debug entries are shared between contexts, so
-	 * this will fail if the process tries to open the driver
-	 * again with a different context. The priting code will
-	 * anyway print all contexts that a given PID has, so this
-	 * is not a problem.
-	 */
-	proc->debugfs_entry = debugfs_create_file(strbuf, S_IRUGO,
-	    binder_debugfs_dir_entry_proc,
-	    (void *)(unsigned long)proc->pid,
-	    &binder_proc_fops);
-    }
+		snprintf(strbuf, sizeof(strbuf), "%u", proc->pid);
+		/*
+		 * proc debug entries are shared between contexts, so
+		 * this will fail if the process tries to open the driver
+		 * again with a different context. The priting code will
+		 * anyway print all contexts that a given PID has, so this
+		 * is not a problem.
+		 */
+		proc->debugfs_entry = debugfs_create_file(strbuf, S_IRUGO,
+			binder_debugfs_dir_entry_proc,
+			(void *)(unsigned long)proc->pid,
+			&binder_proc_fops);
+	}
 
     return 0;
 }
