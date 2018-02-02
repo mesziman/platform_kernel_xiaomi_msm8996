31a5827358ae63d08e8592ddb8cc8ecdf88a5f95
Daniel Rosenberg <drosen@google.com>
2018-02-01 16:52:22 -0800

    ANDROID: sdcardfs: Protect set_top
    If the top is changed while we're attempting to use it, it's
    possible that the reference will be put while we are in the
    process of grabbing a reference.
    
    Now we grab a spinlock to protect grabbing our reference count.
    
    Additionally, we now set the inode_info's top value to point to
    it's own data when initializing, which makes tracking changes
    easier.
    
    Change-Id: If15748c786ce4c0480ab8c5051a92523aff284d2
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -340,14 +340,12 @@ static int sdcardfs_read_super(struct vfsmount *mnt, struct super_block *sb,
 	sb_info->obbpath_s = kzalloc(PATH_MAX, GFP_KERNEL);
 	mutex_lock(&sdcardfs_super_list_lock);
 	if (sb_info->options.multiuser) {
-		setup_derived_state(d_inode(sb->s_root), PERM_PRE_ROOT,
-				sb_info->options.fs_user_id, AID_ROOT,
-				false, SDCARDFS_I(d_inode(sb->s_root))->data);
+		setup_derived_state(sb->s_root->d_inode, PERM_PRE_ROOT,
+				sb_info->options.fs_user_id, AID_ROOT);
 		snprintf(sb_info->obbpath_s, PATH_MAX, "%s/obb", dev_name);
 	} else {
-		setup_derived_state(d_inode(sb->s_root), PERM_ROOT,
-				sb_info->options.fs_user_id, AID_ROOT,
-				false, SDCARDFS_I(d_inode(sb->s_root))->data);
+		setup_derived_state(sb->s_root->d_inode, PERM_ROOT,
+				sb_info->options.fs_user_id, AID_ROOT);
 		snprintf(sb_info->obbpath_s, PATH_MAX, "%s/Android/obb", dev_name);
 	}
 	fixup_tmp_permissions(d_inode(sb->s_root));
