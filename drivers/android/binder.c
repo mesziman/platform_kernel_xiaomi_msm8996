c23759ecc6b95e636c8229e4e1534473b24239b2
Todd Kjos <tkjos@android.com>
2018-02-07 12:38:47 -0800

    binder: remove WARN() for redundant txn error
    binder_send_failed_reply() is called when a synchronous
    transaction fails. It reports an error to the thread that
    is waiting for the completion. Given that the transaction
    is synchronous, there should never be more than 1 error
    response to that thread -- this was being asserted with
    a WARN().
    
    However, when exercising the driver with syzbot tests, cases
    were observed where multiple "synchronous" requests were
    sent without waiting for responses, so it is possible that
    multiple errors would be reported to the thread. This testing
    was conducted with panic_on_warn set which forced the crash.
    
    This is easily reproduced by sending back-to-back
    "synchronous" transactions without checking for any
    response (eg, set read_size to 0):
    
        bwr.write_buffer = (uintptr_t)&bc1;
        bwr.write_size = sizeof(bc1);
        bwr.read_buffer = (uintptr_t)&br;
        bwr.read_size = 0;
        ioctl(fd, BINDER_WRITE_READ, &bwr);
        sleep(1);
        bwr2.write_buffer = (uintptr_t)&bc2;
        bwr2.write_size = sizeof(bc2);
        bwr2.read_buffer = (uintptr_t)&br;
        bwr2.read_size = 0;
        ioctl(fd, BINDER_WRITE_READ, &bwr2);
        sleep(1);
    
    The first transaction is sent to the servicemanager and the reply
    fails because no VMA is set up by this client. After
    binder_send_failed_reply() is called, the BINDER_WORK_RETURN_ERROR
    is sitting on the thread's todo list since the read_size was 0 and
    the client is not waiting for a response.
    
    The 2nd transaction is sent and the BINDER_WORK_RETURN_ERROR has not
    been consumed, so the thread's reply_error.cmd is still set (normally
    cleared when the BINDER_WORK_RETURN_ERROR is handled). Therefore
    when the servicemanager attempts to reply to the 2nd failed
    transaction, the error is already set and it triggers this warning.
    
    This is a user error since it is not waiting for the synchronous
    transaction to complete. If it ever does check, it will see an
    error.
    
    Changed the WARN() to a pr_warn().
    
    Change-Id: I4d2eedd025b77856ee2e77138f2ed7635d312a73
    Signed-off-by: Todd Kjos <tkjos@android.com>
    Reported-by: syzbot <syzkaller@googlegroups.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2112,36 +2112,46 @@ static void binder_free_transaction(struct binder_transaction *t)
 static void binder_send_failed_reply(struct binder_transaction *t,
 		     uint32_t error_code)
 {
-    struct binder_thread *target_thread;
-    struct binder_transaction *next;
+	struct binder_thread *target_thread;
+	struct binder_transaction *next;
 
-    BUG_ON(t->flags & TF_ONE_WAY);
-    while (1) {
-	target_thread = binder_get_txn_from_and_acq_inner(t);
-	if (target_thread) {
-	    binder_debug(BINDER_DEBUG_FAILED_TRANSACTION,
-		     "send failed reply for transaction %d to %d:%d\n",
-		      t->debug_id,
-		      target_thread->proc->pid,
-		      target_thread->pid);
+	BUG_ON(t->flags & TF_ONE_WAY);
+	while (1) {
+		target_thread = binder_get_txn_from_and_acq_inner(t);
+		if (target_thread) {
+			binder_debug(BINDER_DEBUG_FAILED_TRANSACTION,
+				     "send failed reply for transaction %d to %d:%d\n",
+				      t->debug_id,
+				      target_thread->proc->pid,
+				      target_thread->pid);
 
-	    binder_pop_transaction_ilocked(target_thread, t);
-	    if (target_thread->reply_error.cmd == BR_OK) {
-		target_thread->reply_error.cmd = error_code;
-		binder_enqueue_thread_work_ilocked(
-		    target_thread,
-		    &target_thread->reply_error.work);
-		wake_up_interruptible(&target_thread->wait);
-	    } else {
-		WARN(1, "Unexpected reply error: %u\n",
-			target_thread->reply_error.cmd);
-	    }
-	    binder_inner_proc_unlock(target_thread->proc);
-	    binder_thread_dec_tmpref(target_thread);
-	    binder_free_transaction(t);
-	    return;
-	}
-	next = t->from_parent;
+			binder_pop_transaction_ilocked(target_thread, t);
+			if (target_thread->reply_error.cmd == BR_OK) {
+				target_thread->reply_error.cmd = error_code;
+				binder_enqueue_thread_work_ilocked(
+					target_thread,
+					&target_thread->reply_error.work);
+				wake_up_interruptible(&target_thread->wait);
+			} else {
+				/*
+				 * Cannot get here for normal operation, but
+				 * we can if multiple synchronous transactions
+				 * are sent without blocking for responses.
+				 * Just ignore the 2nd error in this case.
+				 */
+				pr_warn("Unexpected reply error: %u\n",
+					target_thread->reply_error.cmd);
+			}
+			binder_inner_proc_unlock(target_thread->proc);
+			binder_thread_dec_tmpref(target_thread);
+			binder_free_transaction(t);
+			return;
+		}
+		next = t->from_parent;
+
+		binder_debug(BINDER_DEBUG_FAILED_TRANSACTION,
+			     "send failed reply for transaction %d, target dead\n",
+			     t->debug_id);
 
 	binder_debug(BINDER_DEBUG_FAILED_TRANSACTION,
 	         "send failed reply for transaction %d, target dead\n",
