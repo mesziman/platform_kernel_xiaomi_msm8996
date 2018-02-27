582a896de1890c36f8307f6d887eb8ab55dee118
Chris Redpath <chris.redpath@arm.com>
2018-02-27 15:29:09 +0000

    sched: Move schedtune en/dequeue before schedutil update triggers
    CPU rq util updates happen when rq signals are updated as part of
    enqueue and dequeue operations. Doing these updates triggers a call to
    the registered util update handler, which takes schedtune boosting
    into account. Enqueueing the task in the correct schedtune group after
    this happens means that we will potentially not see the boost for an
    entire throttle period.
    
    Move the enqueue/dequeue operations for schedtune before the signal
    updates which can trigger OPP changes.
    
    Change-Id: I4236e6b194bc5daad32ff33067d4be1987996780
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4410,27 +4410,27 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
     struct cfs_rq *cfs_rq;
     struct sched_entity *se = &p->se;
 #ifdef CONFIG_SMP
-    int task_new = flags & ENQUEUE_WAKEUP_NEW;
-    int task_wakeup = flags & ENQUEUE_WAKEUP;
+	int task_new = flags & ENQUEUE_WAKEUP_NEW;
+	int task_wakeup = flags & ENQUEUE_WAKEUP;
 
-    /*
-     * Update SchedTune accounting.
-     *
-     * We do it before updating the CPU capacity to ensure the
-     * boost value of the current task is accounted for in the
-     * selection of the OPP.
-     *
-     * We do it also in the case where we enqueue a throttled task;
-     * we could argue that a throttled task should not boost a CPU,
-     * however:
-     * a) properly implementing CPU boosting considering throttled
-     *    tasks will increase a lot the complexity of the solution
-     * b) it's not easy to quantify the benefits introduced by
-     *    such a more complex solution.
-     * Thus, for the time being we go for the simple solution and boost
-     * also for throttled RQs.
-     */
-    schedtune_enqueue_task(p, cpu_of(rq));
+	/*
+	 * Update SchedTune accounting.
+	 *
+	 * We do it before updating the CPU capacity to ensure the
+	 * boost value of the current task is accounted for in the
+	 * selection of the OPP.
+	 *
+	 * We do it also in the case where we enqueue a throttled task;
+	 * we could argue that a throttled task should not boost a CPU,
+	 * however:
+	 * a) properly implementing CPU boosting considering throttled
+	 *    tasks will increase a lot the complexity of the solution
+	 * b) it's not easy to quantify the benefits introduced by
+	 *    such a more complex solution.
+	 * Thus, for the time being we go for the simple solution and boost
+	 * also for throttled RQs.
+	 */
+	schedtune_enqueue_task(p, cpu_of(rq));
 #endif
 
     /*
@@ -4477,24 +4477,24 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	add_nr_running(rq, 1);
 
 #ifdef CONFIG_SMP
-    if (!se) {
-	walt_inc_cumulative_runnable_avg(rq, p);
-	if (!task_new && !rq->rd->overutilized &&
-	    cpu_overutilized(rq->cpu)) {
-	    rq->rd->overutilized = true;
-	    trace_sched_overutilized(true);
-	}
+	if (!se) {
+		walt_inc_cumulative_runnable_avg(rq, p);
+		if (!task_new && !rq->rd->overutilized &&
+		    cpu_overutilized(rq->cpu)) {
+			rq->rd->overutilized = true;
+			trace_sched_overutilized(true);
+		}
 
-	/*
-	 * We want to potentially trigger a freq switch
-	 * request only for tasks that are waking up; this is
-	 * because we get here also during load balancing, but
-	 * in these cases it seems wise to trigger as single
-	 * request after load balancing is done.
-	 */
-	if (task_new || task_wakeup)
-	    update_capacity_of(cpu_of(rq));
-    }
+		/*
+		 * We want to potentially trigger a freq switch
+		 * request only for tasks that are waking up; this is
+		 * because we get here also during load balancing, but
+		 * in these cases it seems wise to trigger as single
+		 * request after load balancing is done.
+		 */
+		if (task_new || task_wakeup)
+			update_capacity_of(cpu_of(rq));
+	}
 
     /* Update SchedTune accouting */
     schedtune_enqueue_task(p, cpu_of(rq));
@@ -4513,9 +4513,9 @@ static void set_next_buddy(struct sched_entity *se);
  */
 static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 {
-    struct cfs_rq *cfs_rq;
-    struct sched_entity *se = &p->se;
-    int task_sleep = flags & DEQUEUE_SLEEP;
+	struct cfs_rq *cfs_rq;
+	struct sched_entity *se = &p->se;
+	int task_sleep = flags & DEQUEUE_SLEEP;
 
 #ifdef CONFIG_SMP
     /*
@@ -4575,27 +4575,27 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	sub_nr_running(rq, 1);
 
 #ifdef CONFIG_SMP
-    if (!se) {
-	walt_dec_cumulative_runnable_avg(rq, p);
+	if (!se) {
+		walt_dec_cumulative_runnable_avg(rq, p);
 
-	/*
-	 * We want to potentially trigger a freq switch
-	 * request only for tasks that are going to sleep;
-	 * this is because we get here also during load
-	 * balancing, but in these cases it seems wise to
-	 * trigger as single request after load balancing is
-	 * done.
-	 */
-	if (task_sleep) {
-	    if (rq->cfs.nr_running)
-		update_capacity_of(cpu_of(rq));
-	    else if (sched_freq())
-		set_cfs_cpu_capacity(cpu_of(rq), false, 0); /* no normalization required for 0 */
+		/*
+		 * We want to potentially trigger a freq switch
+		 * request only for tasks that are going to sleep;
+		 * this is because we get here also during load
+		 * balancing, but in these cases it seems wise to
+		 * trigger as single request after load balancing is
+		 * done.
+		 */
+		if (task_sleep) {
+			if (rq->cfs.nr_running)
+				update_capacity_of(cpu_of(rq));
+			else if (sched_freq())
+				set_cfs_cpu_capacity(cpu_of(rq), false, 0); /* no normalization required for 0 */
+		}
 	}
-    }
 
-    /* Update SchedTune accouting */
-    schedtune_dequeue_task(p, cpu_of(rq));
+	/* Update SchedTune accouting */
+	schedtune_dequeue_task(p, cpu_of(rq));
 
 #endif /* CONFIG_SMP */
 
@@ -5180,7 +5180,6 @@ static int find_new_capacity(struct energy_env *eenv, int cpu_id)
 			break;
 		}
 	}
-    }
 
 	return eenv->cpu[cpu_id].cap_idx;
 }
@@ -5464,7 +5463,6 @@ static inline int select_energy_cpu_idx(struct energy_env *eenv)
 				break;
 		}
 	}
-    }
 
 	return 0;
 }
@@ -6499,7 +6497,7 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
     int next_cpu;
     int delta = 0;
     int cpu = smp_processor_id();
-    struct energy_env eenv;
+    struct energy_env *eenv;
 
     schedstat_inc(p, se.statistics.nr_wakeups_secb_attempts);
     schedstat_inc(this_rq(), eas_stats.secb_attempts);
