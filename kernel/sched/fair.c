eef1a57e46cccb24531513b44da3f2779009d0af
Ionela Voinescu <ionela.voinescu@arm.com>
2018-03-21 09:24:12 +0000

    Revert "sched/fair: use min capacity when evaluating idle backup cpus"
    This reverts commit 88a968ca63f84ecd0747bba67ccab878684bb0a1.
    
    Given the experimental nature of this patch and considering the
    intention to simplify find_best_target, it's best to not introduce
    unnecessary complexity in the heuristics here until the necessity of
    this functionality is proven.
    
    This patch could also potentially introduce a preference towards big CPUs
    for !prefer_idle and boosted tasks, when iterating though CPUs from bigs
    to LITTLES.
    Despite it not being a typical schedtune configuration, this contradicts
    the intention of selecting energy efficient CPUs for !prefer_idle tasks.
    
    Change-Id: I60217c5d8624b375582946b0c4b3660c0517e3f3
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Reported-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reported-by: Leo Yan <leo.yan@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6112,13 +6112,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool boosted, bool prefer_idle)
 {
 	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
+	unsigned long best_idle_min_cap_orig = ULONG_MAX;
 	unsigned long min_util = boosted_task_util(p);
 	unsigned long target_capacity = ULONG_MAX;
 	unsigned long min_wake_util = ULONG_MAX;
 	unsigned long target_max_spare_cap = 0;
 	unsigned long best_active_util = ULONG_MAX;
 	unsigned long target_max_free_util = 0;
-	unsigned long target_idle_max_spare_cap = 0;
 	int best_idle_cstate = INT_MAX;
 	struct sched_domain *sd;
 	struct sched_group *sg;
@@ -6171,7 +6171,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		for_each_cpu_and(i, tsk_cpus_allowed(p), sched_group_cpus(sg)) {
 			unsigned long capacity_curr = capacity_curr_of(i);
 			unsigned long capacity_orig = capacity_orig_of(i);
-			unsigned long wake_util, new_util, real_util, min_capped_util;
+			unsigned long wake_util, new_util, real_util;
 			int idle_idx;
 
 			if (!cpu_online(i))
@@ -6194,15 +6194,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * than the one required to boost the task.
 			 */
 			new_util = max(min_util, real_util);
-
-			/*
-			 * Include minimum capacity constraint:
-			 * new_util contains the required utilization including
-			 * boost. min_capped_util also takes into account a
-			 * minimum capacity cap imposed on the CPU by external
-			 * actors.
-			 */
-			min_capped_util = max(new_util, capacity_min_of(i));
 
 			if (new_util > capacity_orig)
 				continue;
@@ -6344,11 +6335,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 */
 			if (idle_cpu(i)) {
 
-				/* Favor CPUs that won't end up running at a
-				 * high OPP.
-				 */
-				if ((capacity_orig - min_capped_util) <
-					target_idle_max_spare_cap)
+				/* Select idle CPU with lower cap_orig */
+				if (capacity_orig > best_idle_min_cap_orig)
 					continue;
 
 				/*
@@ -6364,8 +6352,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 				/* Keep track of best idle CPU */
 				target_capacity = capacity_orig;
-				target_idle_max_spare_cap = capacity_orig -
-							    min_capped_util;
+				best_idle_min_cap_orig = capacity_orig;
 				best_idle_cstate = idle_idx;
 				best_idle_cpu = i;
 				continue;
@@ -6391,10 +6378,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * capacity and highest free cpu utility.
 			 */
 
+			/* Favor CPUs with smaller capacity */
+			    if (capacity_orig > target_capacity)
+				    continue;
+
 			/* Favor CPUs with maximum spare capacity */
-			if ((capacity_orig == target_capacity) &&
-			    ((capacity_orig - min_capped_util) < target_max_spare_cap))
-				continue;
+			    if ((capacity_orig - new_util) < target_max_spare_cap)
+				    continue;
 
 			target_max_spare_cap = capacity_orig - new_util;
 			target_capacity = capacity_orig;
